7695da8651a92e834a4c52aa02ccc3e75c1fe5bd
Palanbyul <86470977+ssxst31@users.noreply.github.com>
2022-12-20 14:47:47 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,12 @@
+function solution(price) {
+  if (price >= 500000) {
+    return Math.floor(price - price * 0.2)
+  }
+  if (price >= 300000) {
+    return Math.floor(price - price * 0.1)
+  }
+  if (price >= 100000) {
+    return Math.floor(price - price * 0.05)
+  }
+  return price
+}
