7241aad4703b922c1fb875abe276f12e6c188fbe
Palanbyul <86470977+ssxst31@users.noreply.github.com>
2022-12-28 22:17:21 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 14.71 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+  const answer = [arr[0]]
+
+  for (let i = 0; i < arr.length; i++) {
+    if (answer[answer.length - 1] !== arr[i]) {
+      answer.push(arr[i])
+    }
+  }
+
+  return answer
+}
